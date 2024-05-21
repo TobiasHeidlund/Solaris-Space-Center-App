cdc589f7eb66fdbe81c313e5022b5befc7797722
MahmoudAbed2 <148431502+MahmoudAbed2@users.noreply.github.com>
2024-05-21 21:34:24 +0200

    Update FavoritePage.tsx
    updaterat

--- src/pages/FavoritePage.tsx
+++ src/pages/FavoritePage.tsx
@@ -1,21 +1,32 @@
-import PlanetsTitle from '../components/PlanetTitle'
-import PlanetNav from '../components/PlanetNav'
-import PlanetType from '../models/Planet'
-
+import React from 'react';
+import PlanetsTitle from '../components/PlanetTitle';
+import PlanetNav from '../components/PlanetNav';
+import PlanetType from '../models/Planet';
 
 type Props = {
-    planetList : PlanetType[]
+    planetList: PlanetType[]
 }
 
-
-function FavoritePage({ planetList } : Props) {
+function FavoritePage({ planetList }: Props) {
   return (
     <div>
-        <p>FavoritePage</p>
-        <PlanetNav/>
-        <PlanetsTitle/>
+      <p>FavoritePage</p>
+      <PlanetNav/>
+      <PlanetsTitle/>
+      <div>
+        {planetList.length === 0 ? (
+          <p>No favorite planets to display</p>
+        ) : (
+          <ul>
+            {planetList.map((planet) => (
+              <li key={planet.name}>{planet.name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default FavoritePage
+export default FavoritePage;
+
